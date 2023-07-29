39853d8904b27bc8af96289765a2be1141a6fe02
Sevban Bayır <94565457+sevbanBayir@users.noreply.github.com>
2023-07-29 14:28:08 +0300

    Add files via upload

---
+++ ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbayir <sbayir@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/20 00:36:10 by sbayir            #+#    #+#             */
+/*   Updated: 2022/12/20 02:13:17 by sbayir           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	chr;
+	size_t			i;
+
+	chr = (unsigned char)c;
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *)s)[i] == (unsigned char)c)
+			return (((void *)s) + i);
+		i++;
+	}
+	return (NULL);
+}
---
+++ ft_strnstr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbayir <sbayir@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/20 00:13:27 by sbayir            #+#    #+#             */
+/*   Updated: 2022/12/20 00:13:59 by sbayir           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	size_t	needle_len;
+
+	i = 0;
+	needle_len = ft_strlen(needle);
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	while (i < len && haystack[i])
+	{
+		if (haystack[i] == needle[0])
+		{
+			j = 0;
+			while (*(haystack + i + j) == *(needle + j) && i + j < len)
+			{
+				if (!needle[j + 1])
+					return ((char *)(haystack + i));
+				j++;
+			}
+		}
+		i++;
+	}
+	return (NULL);
+}
